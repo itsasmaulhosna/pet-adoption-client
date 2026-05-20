76649a1e5009f50d5ceab84ae88e5a718a4f42d5
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-05-20 09:30:13 +0600

    Created featured

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from '@/components/Banner';
+import FeaturedPet from '@/components/FeaturedPet';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <FeaturedPet />
     </div>
   );
 }
